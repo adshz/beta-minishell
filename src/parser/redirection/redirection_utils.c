f3dfb3004f8d4afafb537432c161f726f49fd63d
shenghongzhong <howdavidlearncoding@gmail.com>
2025-03-04 16:36:42 +0000

    redirection utils dvision

--- src/parser/redirection/redirection_utils.c
+++ src/parser/redirection/redirection_utils.c
@@ -11,36 +11,60 @@
 /* ************************************************************************** */
 #include "parser/parser.h"
 
-static t_ast_node	*setup_redirection_nodes(t_token *current,
+static t_ast_node	*attach_file_to_redirection(t_token *current,
 										t_token *file_token,
-										t_ast_node *result)
+										t_ast_node *redir_node)
 {
-	t_ast_node	*redir_node;
 	t_ast_node	*file_node;
 
-	redir_node = create_redirection_node(current->type, file_token->value);
-	if (!redir_node)
+	if (current->type == TOKEN_HEREDOC)
+		return (redir_node);
+	file_node = create_file_node(file_token, redir_node);
+	if (!file_node)
 		return (NULL);
-	redir_node = track_ast_node(redir_node);
-	if (current->type != TOKEN_HEREDOC)
-	{
-		file_node = create_file_node(file_token, redir_node);
-		if (!file_node)
-			return (NULL);
-		redir_node->right = file_node;
-	}
+	redir_node->right = file_node;
+	return (redir_node);
+}
+
+static t_ast_node	*setup_redirection_left_node(t_ast_node *redir_node,
+										t_ast_node *result)
+{
 	if (!result)
 	{
 		redir_node->left = NULL;
 		return (redir_node);
 	}
-	if (is_redirection_type(result->type))
-	{
-		redir_node->left = result;
-		return (redir_node);
-	}
 	redir_node->left = result;
+	if (is_redirection_type(result->type))
+		return (redir_node);
 	return (redir_node);
+}
+
+static t_ast_node	*create_and_track_redirection(t_token *current,
+										t_token *file_token)
+{
+	t_ast_node	*redir_node;
+
+	redir_node = create_redirection_node(current->type, file_token->value);
+	if (!redir_node)
+		return (NULL);
+	redir_node = track_ast_node(redir_node);
+	return (redir_node);
+}
+
+static t_ast_node	*setup_redirection_nodes(t_token *current,
+										t_token *file_token,
+										t_ast_node *result)
+{
+	t_ast_node	*redir_node;
+
+	redir_node = create_and_track_redirection(current, file_token);
+	if (!redir_node)
+		return (NULL);
+	redir_node = attach_file_to_redirection(current, file_token, redir_node);
+	if (!redir_node)
+		return (NULL);
+	return (setup_redirection_left_node(redir_node, result));
 }
 
 /**
